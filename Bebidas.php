cdf3470cbb410d4abcc27a8a97dedc1b68fb7e3a
brunolive96 <121820969+brunolive96@users.noreply.github.com>
2023-02-13 15:07:29 -0300

    Add files via upload

---
+++ Bebidas.php
@@ -0,0 +1,209 @@
+﻿<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Website</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="#" title="Hambúrguer">
+                    <img src="images/bg.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="sites.html">Página Inicial</a>
+                    </li>
+                    <li>
+                        <a href="categoria.html">Categorias</a>
+                    </li>
+                    <li>
+                        <a href="comidas.html">Comidas</a>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+
+    <section class="food-search text-center">
+        <div class="container">
+            
+            <form action="food-search.html" method="POST">
+                <input type="Search" name="Procurar" placeholder="Procurar Bebidas.." required>
+                <input type="submit" name="submit" value="Procurar" class="btn btn-primary">
+            </form>
+
+        </div>
+    </section>
+
+    <section class="food-menu">
+        <div class="container">
+            <h2 class="text-center">Bebidas</h2> 
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/Cerveja-Budweiser.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Cerveja-Budweiser</h4>
+                    <p class="food-price">R$12.99</p>
+                    <p class="food-detail">Cerveja-Budweiser 600ML.</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/brahma-600.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Brahma-600</h4>
+                    <p class="food-price">R$13.99</p>
+                    <p class="food-detail">Brahma-Garrafa 350ML.</p> 
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/cerveja-amstel-lata.png" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>cerveja-amstel-lata</h4>
+                    <p class="food-price">R$12.50</p>
+                    <p class="food-detail">cerveja-amstel-lata 350ML</p>
+                    <br>
+                    <br> 
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/Coca-Cola-1-Litro1.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Coca-Cola-1-Litro1</h4>
+                    <p class="food-price">R$10.99</p>
+                    <p class="food-detail">Coca-Cola-Garrafa 1Litro</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/refrigerantes-lata-1.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>refrigerantes-lata</h4>
+                    <p class="food-price">R$06.50</p>
+                    <p class="food-detail">refrigerantes-lata- 1 und.</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/agua-mineral-cristal-sem-gas-500-ml.jpg" alt="Chicke Hawain Burger" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>agua-mineral-cristal-sem-gas</h4>
+                    <p class="food-price">R$03.50</p>
+                    <p class="food-detail">agua-mineral-cristal-sem-gas-500-ml.</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/Água Mineral Natural Crystal com Gás 500ml.jpg" alt="Chicke Hawain Momo" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4> Água Mineral Natural Crystal com Gás 500ml</h4>
+                    <p class="food-price">R$04.00</p>
+                    <p class="food-detail">Água Mineral Natural Crystal com Gás 500ml.</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/heineken-long-neck-330ml.png" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>heineken-long-neck-330ml</h4>
+                    <p class="food-price">R$11.99</p>
+                    <p class="food-detail">heineken-long-neck-330ml.</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+
+    </section>
+
+    <section class="social">
+        <div class="container text-center">
+
+        </div>
+    </section>
+
+    <section class="footer">
+        <div class="container text-center">
+            <p> <a href="#"></a></p>
+        </div>
+    </section>
+
+</body>
+</html>
---
+++ Hambúrguers.php
@@ -0,0 +1,177 @@
+﻿<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Website</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="#" title="Hambúrguer">
+                    <img src="images/bg.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="sites.html">Página Inicial</a>
+                    </li>
+                    <li>
+                        <a href="categoria.html">Categorias</a>
+                    </li>
+                    <li>
+                        <a href="comidas.html">Comidas</a>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+
+    <section class="food-search text-center">
+        <div class="container">
+            
+            <form action="food-search.html" method="POST">
+                <input type="Search" name="Procurar" placeholder="Procurar Sabores.." required>
+                <input type="submit" name="submit" value="Procurar" class="btn btn-primary">
+            </form>
+
+        </div>
+    </section>
+
+    <section class="food-menu">
+
+        <div class="container">
+            <h2 class="text-center">Hambúrguer</h2>
+
+
+                <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/Mega Combo Hamburguer Batata Frita Coca Cola.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Mega Combo Hamburguer Batata Frita Coca Cola</h4>
+                    <p class="food-price">R$15.99</p>
+                    <p class="food-detail">Mega Combo Hamburguer Batata Frita Coca Cola.</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/hamburguer.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>hamburguer</h4>
+                    <p class="food-price">R$13.99</p>
+                    <p class="food-detail">hamburguer.</p> 
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/Hambúrguer Angus com Fatias Redondas de Bacon.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Hambúrguer Angus com Fatias Redondas de Bacon</h4>
+                    <p class="food-price">R$21.50</p>
+                    <p class="food-detail">Hambúrguer Angus com Fatias Redondas de Bacon</p>
+                    <br>
+                    <br> 
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/Hambúrguer Duplo com Maionese Chipotle.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Hambúrguer Duplo com Maionese Chipotle</h4>
+                    <p class="food-price">R$22.99</p>
+                    <p class="food-detail">Hambúrguer Duplo com Maionese Chipotle</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/Hambúrguer com bacon e cheddar.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Hambúrguer com bacon e cheddar</h4>
+                    <p class="food-price">R$18.50</p>
+                    <p class="food-detail">Hambúrguer com bacon e cheddar.</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="food-menu-box" style="height: 170px;">
+
+                <div class="food-menu-img">
+                    <img src="images/Hambúrguer Combo Batata Frita.jpg" alt="Chicke Hawain Burger" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Hambúrguer Combo Batata Frita</h4>
+                    <p class="food-price">R$14.50</p>
+                    <p class="food-detail">Hambúrguer Combo Batata Frita.</p>
+                    <br>
+                    <br>
+                    <a href="ordem.html" class="btn btn-primary">comfirmar pedido</a>
+                </div>
+
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+
+    </section>
+
+    <section class="social">
+        <div class="container text-center">
+
+        </div>
+    </section>
+
+    <section class="footer">
+        <div class="container text-center">
+            <p> <a href="#"></a></p>
+        </div>
+    </section>
+
+</body>
+</html>
